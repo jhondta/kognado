270f851f2dc55caef9f6fb2eb02dcf29c0f9563c
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2023-12-27 23:51:12 -0600

    Adds authenticate_user before_action

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,6 +2,8 @@
 
 # PublicController is a controller for static pages that are publicly accessible.
 class PublicController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def home; end
 
   def help; end
