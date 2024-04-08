79242cbbee5978e5b84af5e07e7e242e3c1355c0
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2024-04-07 20:12:07 -0600

    First Flowbite styled flash.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@
 
 # Application helper
 module ApplicationHelper
+  # Returns the color for the alert
+  def alert_color(type)
+    colors = { 'notice' => 'blue',
+               'alert' => 'red',
+               'error' => 'red',
+               'success' => 'green',
+               'warning' => 'yellow' }
+    colors[type] || 'blue'
+  end
 end
