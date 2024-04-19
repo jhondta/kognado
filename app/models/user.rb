452d45764c08704ec168ec7d6295bc45be6fdb15
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2024-04-18 18:56:31 -0600

    Adds a user full name method and user menu in navigation bar.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :argon2,
          :registerable, :recoverable, :rememberable,
          :validatable, :confirmable, :lockable, :timeoutable, :trackable
+
+  # Shows the full name of the user
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
