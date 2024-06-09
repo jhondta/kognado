60e1247744cf06d454870ba9cd59beb68de51035
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2024-06-09 17:43:43 -0600

    Adss utitlity methods to application and user model.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,12 @@
 
 # Application helper
 module ApplicationHelper
+  # Returns the full title on a per-page basis
+  def full_title(page_title = '')
+    base_title = 'Kognado'
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
+
   # Returns the color for the alert
   def alert_color(type)
     colors = { 'notice' => 'blue',
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,21 @@ class User < ApplicationRecord
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  # Returns the user's initials
+  # Example: John Doe => JD
+  def initials
+    "#{first_name[0]}#{last_name[0]}"
+  end
+
+  # Returns the user's avatar.
+  # If the user has an avatar, it will return the avatar.
+  # If the user does not have an avatar, it will return the user's initials.
+  def avatar
+    if avatar.attached?
+      avatar
+    else
+      initials
+    end
+  end
 end
