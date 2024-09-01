604c8f88c61dea701b361a52fa76bf814cd9a6c7
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2024-09-01 15:23:08 -0600

    Adds Flowbite served locally

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,4 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin 'flowbite', to: 'https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.turbo.min.js'
+pin 'flowbite', to: 'flowbite.turbo.min.js'
