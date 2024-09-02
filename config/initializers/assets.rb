0b292cb63ca2b29eca33e2595c086a12b26ac3c8
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2024-09-02 12:02:10 -0600

    Removes redundant default load paths declarations from assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,5 +7,3 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-Rails.application.config.assets.paths << Rails.root.join('app', 'javascript')
