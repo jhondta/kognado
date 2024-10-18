35e4d2b815fce1497ed4f5827372b9982c5f55b7
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2024-10-18 13:39:32 -0600

    Watches all stylesheets under /app/assets/stylesheets to be rebuilt on change.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,24 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
-      },
+    content: [
+        './public/*.html',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/views/**/*.{erb,haml,html,slim}',
+        './app/assets/stylesheets/**/*.css',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ],
-  darkMode: 'class',
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/container-queries'),
+    ],
+    darkMode: 'class',
 }
