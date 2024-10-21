b3117a62e6c42ada9245c9c7c4c31212a857a395
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2024-10-21 01:15:14 -0600

    Adds attached image to Inventory::Item.

--- app/models/inventory/item.rb
+++ app/models/inventory/item.rb
@@ -5,6 +5,7 @@ class Inventory::Item < ApplicationRecord
   belongs_to :measure_unit
   has_many :item_categories, class_name: 'Inventory::ItemCategory', foreign_key: :inventory_item_id
   has_many :categories, through: :item_categories
+  has_one_attached :image
 
   # Validations
   validates :name, :sku, :description, presence: true
