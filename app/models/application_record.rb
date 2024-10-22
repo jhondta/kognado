a1ca815f6455be4f63508d4437a804da2ad94c2e
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2024-10-21 18:04:24 -0600

    Adds method to translate enums in all models.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,4 +3,19 @@
 # Application record
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  # Returns the translated enum value
+  # @param enum_field [Symbol]
+  # @return [String]
+  def translate_enum(enum_field)
+    return nil unless send(enum_field)
+
+    I18n.t(
+      "activerecord.enums.#{self.class.model_name.i18n_key}.#{enum_field}.#{send(enum_field)}",
+      default: send(enum_field).to_s.humanize
+    )
+  end
+
+  # Alias más corto si prefieres una sintaxis más concisa
+  alias_method :t_enum, :translate_enum
 end
