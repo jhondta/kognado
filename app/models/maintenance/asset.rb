897d6f7e779678162c87ef49d5d7ac7d325e9eca
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2024-10-22 22:10:46 -0600

    Improves index view for assets.

--- app/models/maintenance/asset.rb
+++ app/models/maintenance/asset.rb
@@ -2,18 +2,30 @@
 
 class Maintenance::Asset < ApplicationRecord
   # Constants
-  STATUS = { inactive: 'inactive', active: 'active', maintenance: 'maintenance',
-             retired: 'retired' }.freeze
-  CRITICALITY_LEVEL = { low: 'low', medium: 'medium', high: 'high',
-                        critical: 'critical' }.freeze
+  STATUS = %i[ active inactive maintenance retired ].freeze
+  STATUS_COLORS = { active: 'green', inactive: 'red', maintenance: 'yellow',
+                    retired: 'gray' }.freeze
+  CRITICALITY_LEVEL = %i[ low medium high critical ].freeze
+  CRITICALITY_COLORS = { low: 'green', medium: 'yellow', high: 'orange',
+                         critical: 'red' }.freeze
 
   # Associations
-  belongs_to :asset_type, class_name: 'Maintenance::AssetType',
+  belongs_to :type, class_name: 'Maintenance::AssetType',
              foreign_key: :maintenance_asset_type_id
   belongs_to :area, class_name: 'Configuration::Area',
              foreign_key: :configuration_area_id
   belongs_to :manufacturer, class_name: 'Maintenance::Manufacturer',
              foreign_key: :maintenance_manufacturer_id
+
+  has_many :maintenance_asset_assignees, class_name: 'Maintenance::AssetAssignee',
+           foreign_key: :maintenance_asset_id
+  has_many :asset_components, class_name: 'Maintenance::AssetComponent',
+           foreign_key: :maintenance_asset_id
+  has_many :asset_documents, class_name: 'Maintenance::AssetDocument',
+           foreign_key: :maintenance_asset_id
+  has_many :assignees, through: :maintenance_asset_assignees
+  has_many :components, through: :asset_components
+  has_many :documents, through: :asset_documents
 
   # Validations
 
@@ -27,4 +39,20 @@ class Maintenance::Asset < ApplicationRecord
 
   # Callbacks
   has_paper_trail
+
+  # Methods
+
+  # Returns the status color
+  # @return [String]
+  #
+  def status_color
+    STATUS_COLORS[status.to_sym]
+  end
+
+  # Returns the criticality color
+  # @return [String]
+  #
+  def criticality_color
+    CRITICALITY_COLORS[criticality_level]
+  end
 end
--- app/models/maintenance/asset_assignee.rb
+++ app/models/maintenance/asset_assignee.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Maintenance::AssetAssignee < ApplicationRecord
+  # Associations
   belongs_to :maintenance_asset
   belongs_to :maintenance_technician
 end
